faa4cfb06532786652ba81ba0a95c4362ca7e588
ntarunmenon <ntarunmenon@gmail.com>
2019-01-27 12:09:23 +1000

    routing added but not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+const appRoutes: Routes = [
+  { path: 'employees', component: MainSectionComponent },
+  { path: 'office-locations', component: MainSectionComponent },
+  { path: 'audit-trail', component: MainSectionComponent },
+  { path: '',   redirectTo: '/employees', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
+    MainSectionComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]

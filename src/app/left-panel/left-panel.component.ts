496e18a98182c8f6e1d430a5bd6eb6ee7bf325eb
ntarunmenon <ntarunmenon@gmail.com>
2019-03-04 04:46:48 +1000

    Adding logout

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-left-panel',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService,
+    private router:Router) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.loginService.logout();
+    this.router.navigate(["/"]);
   }
 
 }
